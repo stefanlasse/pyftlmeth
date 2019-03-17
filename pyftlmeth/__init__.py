86d12070bdbf72ef3c42d365b8c04f540a029040
gsec <o0v0o.ix@gmail.com>
2019-03-17 20:07:33 +0100

    __init__.py: classes available in namespace

---
+++ pyftlmeth/__init__.py
@@ -0,0 +1,9 @@
+from .FourierMethods import (FourierTransform,
+                            InverseFourierTransform,
+                            CrossCorrelation,
+                            AutoCorrelation,
+                            Convolution,
+                            PowerSpectralDensity,
+                            )
+from .TimeSeries import TimeSeries
+from .Spectrum import Spectrum
